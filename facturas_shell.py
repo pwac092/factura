321e7ff23485cf2c0e8cb64f8b5b924444d28424
pwac092 <h.j.canizavierci@cs.rhul.ac.uk>
2016-03-04 08:27:30 -0300

    Added default values

--- facturas_shell.py
+++ facturas_shell.py
@@ -4,15 +4,17 @@ from babel.dates import format_date, format_datetime, format_time
 import subprocess
 from num2words import num2words
 
+
+
 class sales():
     def __init__(self):
         #[(concept,five,ten,excempt),...]
         self.concept = ""
-        new_sale.quantity = 0
-        new_sale.price = 0
-        new_sale.five = 0 
-        new_sale.ten = 0
-        new_sale.excempt = 0
+        self.quantity = 0
+        self.price = 0
+        self.five = 0 
+        self.ten = 0
+        self.excempt = 0
 
 class invoice():
     def __init__ (self):
@@ -49,8 +51,8 @@ def print_header(filename):
             """)
 
 
-def print_invoice(destination, invoice):
-    filename = destination + invoice.date.replace(' ','_') + ".tex"
+def print_invoice(filename, invoice):
+
     with open(filename, 'a') as f:
         #date
         f.write("\\lbl[l]{36,256;\\textcolor{black}{\\small \\textbf{ "+ invoice.date+"}}}\n")
@@ -87,23 +89,24 @@ def print_invoice(destination, invoice):
             f.write("\\lbl[l]{125,232;\\textcolor{black}{\\small{"+str(sale.price) +"}}}\n")
             f.write("\\lbl[l]{125,94.3;\\textcolor{black}{\\small{"+str(sale.price)+"}}}\n")
 
-            if sale.concept[2]:
-                total_five += int(sale.concept[2])
-                if int(sale.concept[2]) > 0:
+
+            if sale.five:
+                total_five += int(sale.five)
+                if int(sale.five) > 0:
                     #five percent
                     f.write("\\lbl[l]{156.2,232;\\textcolor{black}{\\small{" + str(sale.five) + "}}}\n")
                     f.write("\\lbl[l]{156.2,94.3;\\textcolor{black}{\\small{" + str(sale.five) + "}}}\n")
 
-            if sale.concept[3]:
-                total_ten += int(sale.concept[3])
-                if int(sale.concept[3]) > 0:
+            if sale.ten:
+                total_ten += int(sale.ten)
+                if int(sale.ten) > 0:
                     #ten percent
                     f.write("\\lbl[l]{170.2,232;\\textcolor{black}{\\small{"+str(sale.ten)+"}}}\n")
                     f.write("\\lbl[l]{170.2,94.3;\\textcolor{black}{\\small{"+str(sale.ten)+"}}}\n")
 
-            if sale.concept[4]:
-                total_excempt += int(sale.concept[4])
-                if int(sale.concept[4]) > 0:
+            if sale.excempt:
+                total_excempt += int(sale.excempt)
+                if int(sale.excempt) > 0:
                     #exentas
                     f.write("\\lbl[l]{140.2,232;\\textcolor{black}{\\small{"+str(sale.excempt)+"}}}\n")
                     f.write("\\lbl[l]{140.2,94.3;\\textcolor{black}{\\small{"+str(sale.excempt) +"}}}\n")
@@ -138,33 +141,21 @@ def print_invoice(destination, invoice):
         f.write("\\lbl[l]{136,37.7;\\textcolor{black}{\\small{"+ str(round((total_five * 0.05) +(total_ten * 0.1)))+"}}}\n")
 
 
-    f.write("\\end{lpic}\n")
-    f.write("\\end{document}\n")
-    #complie the file and open to show it.
 
 
-#verify data.
-def verify(invoice):
-    for sale in invoice.sales:
-        if price*quantity < five + ten + excempt: 
-            ERROR.
-
-        new_sale.price = input('Unit price: ')
-        #new_sale.five = input('Taxed 5%: ') 
-        new_sale.five = 0 #this wil usually be 0, so fix it for now.
-        new_sale.ten = input('Taxed 10%: ')
-        new_sale.excempt = input('Excempt: ')
-
-
-
+def print_trailer(filename):
+    with open(filename, 'a') as f:
+        f.write("\\end{lpic}\n")
+        f.write("\\end{document}\n")
+        #complie the file and open to show it.
 
 #print data
 def produce_invoice(destination, invoice):
     filename = destination + invoice.date.replace(' ','_') + ".tex"
     print_header(filename)
     #finish the file and compile it.
-    print_invoice(todays_date, new_invoice)
-    print_trailer(todays_date)
+    print_invoice(filename, new_invoice)
+    print_trailer(filename)
 
 
 #input data
@@ -173,30 +164,66 @@ if __name__ == "__main__":
     new_invoice = invoice()
 
     #read the data. This could come from the console.
-    new_invoice.name = input('Nombre: ')
-    new_invoice.ruc = input('RUC: ')
-    new_invoice.address = input('Direccion: ')
-    new_invoice.phone = input('Telefono: ')
-    new_invoice.date = format_date(date.today(), locale='es', format='long')
+    new_invoice.name = input('Nombre: (Universidad Paraguayo Alemana)')
+    if new_invoice.name == "":
+        new_invoice.name = 'Universidad Paraguayo Alemana'
+
+    new_invoice.ruc = input('RUC: (123123123) ')
+    if new_invoice.ruc == "":
+        new_invoice.ruc = '2123123123'
+
+    new_invoice.address = input('Direccion: (Lope de Vega esq. Atilio Galfre. San Lorenzo)')
+    if new_invoice.address == "":
+        new_invoice.address = "Lope de Vega esq. Atilio Galfre. San Lorenzo"
+
+    new_invoice.phone = input('Telefono: (021 514 597)')
+    if new_invoice.phone == "":
+        new_invoice.phone= "021 514 597"
+
+    todays_date = input('Todays date?(Y/N):')
+
+    if todays_date.upper() == 'N':
+        print('Date format:')
+        print(format_date(date.today(), locale='es', format='long'))
+        new_invoice.date = input('Input date:')
+    else:
+        new_invoice.date = format_date(date.today(), locale='es', format='long')
+
     new_invoice.sale_type = 'CONTADO'
 
     #process sales
     while(True):
         new_sale = sales()
+
         new_sale.quantity = 1 #this will be usually 1. So fix it for now.
-        new_sale.concept = input('Concept: ')
-        new_sale.price = input('Unit price: ')
+
+        new_sale.concept = input('Concept: (Servicios de docencia) ')
+        if new_sale.concept == "":
+            new_sale.concept = "Servicios de docencia"
+
+
+        new_sale.price = input('Unit price: (18.000.000)')
+        if new_sale.price == "":
+            new_sale.price = "18000000"
+
         #new_sale.five = input('Taxed 5%: ') 
-        new_sale.five = 0 #this wil usually be 0, so fix it for now.
-        new_sale.ten = input('Taxed 10%: ')
-        new_sale.excempt = input('Excempt: ')
+        new_sale.five = '0' #this wil usually be 0, so fix it for now.
+        if new_sale.five == "":
+            new_sale.five = '0'
+
+        new_sale.ten = input('Taxed 10%: (0)')
+        if new_sale.ten == "":
+            new_sale.ten = '0'
+
+        new_sale.excempt = input('Excempt: (18.000.000)')
+        if new_sale.excempt == "":
+            new_sale.excempt = '18000000'
 
         new_invoice.add_sale(new_sale)
-        
-        cont = input('Add another entry?: ')
-        if cont.upper() == 'N':
-            verify('./invoices/', invoice)
-            print_invoice('./invoices/', invoice)
-            exit()
 
-    
+        cont = input('Add another entry? (Y/N): (N) ')
+
+        if cont.upper() == 'N' or cont == '':
+            #verify(invoice)
+            produce_invoice('./invoices/', new_invoice)
+            exit()
